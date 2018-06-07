0192950e70bf6317e5e6d69766a8c61a2310e4ca
Thibaultgroom <thibault.groommy@gmail.com>
2018-06-07 18:36:50 +0200

    07/06 slide1 opé Autres slide en état d'édition

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+<script type="text/javascript"
+    src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js">
+</script>
+<script type="text/javascript" src="script/jquery.parallax-1.1.js"></script>
+
+
+$(document).ready(function(){
+    $('#slide1').parallax("center", 0, 0.1, true);
+})
+</script>
